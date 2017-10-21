5638e6855316483a28144cc48ef4177ac5b53875
mistychris6 <31109593+mistychris6@users.noreply.github.com>
2017-10-20 18:33:24 -0600

    Index JS commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import Routes from './Routes'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
