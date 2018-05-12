0fde6e97c74cd275d87239e9f3c332fe3256fddd
mistychris6 <mistychris6@gmail.com>
2018-05-11 20:30:23 -0600

    Fixed file paths due to file restructure

--- src/Layout/App.js
+++ src/Layout/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import me from './me.JPG'
+import React, { Component } from 'react'
+import logo from '../Images/logo.svg'
+import './App.css'
+import me from '../Images/me.JPG'
 
 
 
-import { BrowserRouter as Router, 
-  Route, 
-  Switch 
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
 } from 'react-router-dom'
 
-import Home from './pages/Home'
-import Work from './pages/Work'
-import Contact from './pages/Contact'
-import About from './pages/About'
-import Projects from './pages/Projects'
-import NotFound from './pages/NotFound'
+import Home from '../Views/Home'
+import Work from '../Views/Work'
+import Contact from '../Views/Contact'
+import About from '../Views/About'
+import Projects from '../Views/Projects'
+import NotFound from '../Views/NotFound'
 
 
 class App extends Component {
@@ -26,22 +27,22 @@ class App extends Component {
           <img src={me} className="App-logo" alt="logo" />
           <h1 className="App-title">MistySue Christiansen</h1>
           <section className="pageLinks">
-          <a href="/">Home</a>
-          <a href="/about">About</a>
-          <a href="/projects">Projects</a>
-          <a href="/work">Work</a>
-          <a href="/contact">Contact</a>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/projects">Projects</a>
+            <a href="/work">Work</a>
+            <a href="/contact">Contact</a>
           </section>
         </header>
         <div className="App-intro">
           <Router>
             <Switch>
-              <Route exact path = "/" component = {Home} />
-              <Route exact path = "/work" component = {Work} />
-              <Route exact path = "/about" component = {About} />
-              <Route exact path = "/projects" component = {Projects} />
-              <Route exact path = "/contact" component = {Contact} />
-              <Route component = {NotFound} />
+              <Route exact path="/" component={Home} />
+              <Route exact path="/work" component={Work} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/projects" component={Projects} />
+              <Route exact path="/contact" component={Contact} />
+              <Route component={NotFound} />
             </Switch>
           </Router>
         </div>
